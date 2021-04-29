676627f8e3681979c8d2ba8eea8cb6df446a6c3b
FlutLab.io <>
2021-04-29 23:03:35 +0000

    Autosave

--- lib/mock.dart
+++ lib/mock.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+Color primaryGreen = Color(0xff416d6d);
+List<BoxShadow> shadowList = [
+  BoxShadow(color: Colors.grey[300], blurRadius: 30, offset: Offset(0, 10))
+];
+
 List<Map> foodCategories = [
   {
     'foodName': 'offers',
