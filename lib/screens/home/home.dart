f7340a957f2e9ad0b3a8e12b3619356e7250bbd3
FlutLab.io <>
2021-04-29 22:38:46 +0000

    Autosave

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,16 +6,33 @@ class Home extends StatelessWidget {
     return Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
         child: SingleChildScrollView(
-            child: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text(
-              "Good morning Akila!",
-              style: TextStyle(
-                fontSize: 20,
+            child: Column(
+          children: [
+            //navbar
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(
+                "Good morning Akila!",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Icon(Icons.shopping_cart)
+            ]),
+            //search bar
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              margin: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.search),
+                  Text('Search pet to adopt'),
+                  Icon(Icons.settings)
+                ],
               ),
             ),
-            Icon(Icons.shopping_cart)
-          ])
-        ])));
+          ],
+        )));
   }
 }
