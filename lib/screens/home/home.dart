124fa3d02e6620f0948fa220d6de69ad4962b23b
FlutLab.io <>
2021-04-29 23:01:01 +0000

    Autosave

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -42,7 +42,7 @@ class Home extends StatelessWidget {
               height: 120,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
+                itemCount: foodCategories.length,
                 itemBuilder: (context, index) {
                   return Container(
                     child: Column(
@@ -52,13 +52,13 @@ class Home extends StatelessWidget {
                           margin: EdgeInsets.only(left: 20),
                           decoration: BoxDecoration(color: Colors.white, boxShadow: shadowList, borderRadius: BorderRadius.circular(10)),
                           child: Image.asset(
-                            categories[index]['iconPath'],
+                            foodCategories[index]['coverPicture'],
                             height: 50,
                             width: 50,
                             color: Colors.grey[700],
                           ),
                         ),
-                        Text(categories[index]['name'])
+                        Text(foodCategories[index]['foodName'])
                       ],
                     ),
                   );
