65b950ad92b28e3c8d5ceea97a8bc508095734f2
FlutLab.io <>
2021-04-29 21:50:54 +0000

    Autosave

---
+++ lib/mock.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+List<Map> foodCategories = [
+  {
+    'foodName': 'offers',
+    'coverPicture': '',
+  },
+  {
+    'foodName': 'Sri Lankan',
+    'coverPicture': '',
+  },
+  {
+    'foodName': 'Italian',
+    'coverPicture': '',
+  },
+  {
+    'foodName': 'Indian',
+    'coverPicture': ''
+  }
+];
