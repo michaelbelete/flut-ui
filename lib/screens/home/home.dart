0dfd9c11f9ce928d18176ea9c087c05eaa5ae8c6
FlutLab.io <>
2021-04-29 22:48:08 +0000

    created search bar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// import 'p'
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -36,33 +37,33 @@ class Home extends StatelessWidget {
             ),
 
             // list view search
-            Container(
-              height: 120,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.only(left: 20),
-                          decoration: BoxDecoration(color: Colors.white, boxShadow: shadowList, borderRadius: BorderRadius.circular(10)),
-                          child: Image.asset(
-                            categories[index]['iconPath'],
-                            height: 50,
-                            width: 50,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                        Text(categories[index]['name'])
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
+            // Container(
+            //   height: 120,
+            //   child: ListView.builder(
+            //     scrollDirection: Axis.horizontal,
+            //     itemCount: categories.length,
+            //     itemBuilder: (context, index) {
+            //       return Container(
+            //         child: Column(
+            //           children: [
+            //             Container(
+            //               padding: EdgeInsets.all(10),
+            //               margin: EdgeInsets.only(left: 20),
+            //               decoration: BoxDecoration(color: Colors.white, boxShadow: shadowList, borderRadius: BorderRadius.circular(10)),
+            //               child: Image.asset(
+            //                 categories[index]['iconPath'],
+            //                 height: 50,
+            //                 width: 50,
+            //                 color: Colors.grey[700],
+            //               ),
+            //             ),
+            //             Text(categories[index]['name'])
+            //           ],
+            //         ),
+            //       );
+            //     },
+            //   ),
+            // ),
           ],
         )));
   }
