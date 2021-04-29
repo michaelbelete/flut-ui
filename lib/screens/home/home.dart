cd135e2670790c6ecd28223f42804bef0e8f5788
FlutLab.io <>
2021-04-29 22:33:00 +0000

    Autosave

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -4,7 +4,13 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
-    );
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: SingleChildScrollView(
+            child: Column(children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text("Good morning Akila!"),
+            Icon(Icons.shopping_cart)
+          ])
+        ])));
   }
 }
